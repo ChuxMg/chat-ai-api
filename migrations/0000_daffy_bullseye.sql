d7d0edac58de7cb2471e50f20fe66ea6ff992d9d
ChuxMg <chuxmgbojikwe@gmail.com>
2025-06-05 00:35:55 +0700

    Implement Backend with Neon PostgreSQL, Drizzle Schema & Database Migration

---
+++ migrations/0000_daffy_bullseye.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "chats" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"user_id" text NOT NULL,
+	"message" text NOT NULL,
+	"reply" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"user_id" text NOT NULL,
+	"email" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
---
+++ migrations/0001_opposite_swarm.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "users" ADD PRIMARY KEY ("user_id");--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "name" text NOT NULL;
